1ec846f0bd0cab8576c8338a06c083ef5dff8281
Jay Greco <jayv.greco@gmail.com>
2022-09-13 05:53:11 +0000

    Add RP2040 SCRAMBLE v2

---
+++ keyboards/nullbitsco/scramble/scramble_led.c
@@ -0,0 +1,111 @@
+/* Copyright 2021 Jay Greco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "scramble_led.h"
+
+#if defined(MCU_RP)
+void set_scramble_LED_R(uint8_t mode) {
+    switch(mode) {
+        case LED_ON:
+            setPinOutput(PIN_LED_R);
+            writePin(PIN_LED_R, GPIO_STATE_LOW);
+        break;
+
+        case LED_DIM:
+            setPinInputLow(PIN_LED_R);
+        break;
+
+        case LED_OFF:
+            setPinOutput(PIN_LED_R);
+            writePin(PIN_LED_R, GPIO_STATE_HIGH);
+        break;
+
+        default:
+        break;
+    }
+}
+void set_scramble_LED_G(uint8_t mode) {
+    switch(mode) {
+        case LED_ON:
+            setPinOutput(PIN_LED_G);
+            writePin(PIN_LED_G, GPIO_STATE_LOW);
+        break;
+
+        case LED_DIM:
+            setPinInputLow(PIN_LED_G);
+        break;
+
+        case LED_OFF:
+            setPinOutput(PIN_LED_G);
+            writePin(PIN_LED_G, GPIO_STATE_HIGH);
+        break;
+
+        default:
+        break;
+    }
+}
+void set_scramble_LED_B(uint8_t mode) {
+    switch(mode) {
+        case LED_ON:
+            setPinOutput(PIN_LED_B);
+            writePin(PIN_LED_B, GPIO_STATE_LOW);
+        break;
+
+        case LED_DIM:
+            setPinInputLow(PIN_LED_B);
+        break;
+
+        case LED_OFF:
+            setPinOutput(PIN_LED_B);
+            writePin(PIN_LED_B, GPIO_STATE_HIGH);
+        break;
+
+        default:
+        break;
+    }
+}
+
+void set_scramble_LED_RGB(uint8_t r_mode, uint8_t g_mode, uint8_t b_mode) {
+    set_scramble_LED_R(r_mode);
+    set_scramble_LED_G(g_mode);
+    set_scramble_LED_B(b_mode);
+}
+
+void set_scramble_LED(uint8_t mode) {
+    set_scramble_LED_RGB(mode, mode, mode);
+}
+
+#else
+void set_scramble_LED(uint8_t mode) {
+    switch(mode) {
+        case LED_ON:
+            setPinOutput(PIN_LED);
+            writePin(PIN_LED, GPIO_STATE_HIGH);
+        break;
+
+        case LED_DIM:
+            setPinInput(PIN_LED);
+        break;
+
+        case LED_OFF:
+            setPinOutput(PIN_LED);
+            writePin(PIN_LED, GPIO_STATE_LOW);
+        break;
+
+        default:
+        break;
+    }
+}
+#endif // MCU_RP
--- keyboards/nullbitsco/scramble/scramble_led.h
+++ keyboards/nullbitsco/scramble/scramble_led.h
@@ -13,16 +13,29 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
 #include "quantum.h"
-#include "scramble_led.h"
 
-#define LAYOUT( \
-    K01, K02, K03, \
-    K11, K12, K13  \
-) { \
-   {K01, K02, K03}, \
-   {K11, K12, K13}, \
-}
+#define LED_ON          2
+#define LED_DIM         1
+#define LED_OFF         0
+
+#define GPIO_STATE_LOW  0
+#define GPIO_STATE_HIGH 1
+
+#if defined(MCU_RP)
+#define PIN_LED_R       GP18
+#define PIN_LED_G       GP19
+#define PIN_LED_B       GP20
+#else // MCU_AVR
+#define PIN_LED         B2
+#endif // defined(MCU_RP)
+
+void set_scramble_LED(uint8_t mode);
+#if defined(MCU_RP)
+void set_scramble_LED_R(uint8_t mode);
+void set_scramble_LED_G(uint8_t mode);
+void set_scramble_LED_B(uint8_t mode);
+void set_scramble_LED_RGB(uint8_t r_mode, uint8_t g_mode, uint8_t b_mode);
+#endif // MCU_RP
